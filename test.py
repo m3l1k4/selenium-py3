5158acff57500d9178d268bdde48b2386c73b671
m3l1k4 <melikaihelas@gmail.com>
2021-01-25 14:22:59 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 #https://sites.google.com/a/chromium.org/chromedriver/downloads
+#/usr/local/bin
+import selenium
+import os
 
-import selenium 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromedriver = '/usr/local/bin/chromedriver'
+# browser = webdriver.Chrome(chromedriver)
