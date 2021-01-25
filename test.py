0d84c060c7423167166cf5b652a9da5888b5493c
m3l1k4 <melikaihelas@gmail.com>
2021-01-25 14:34:45 -0800

    Bad CPU type in executable: '/usr/local/bin/chromedriver'

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-#https://sites.google.com/a/chromium.org/chromedriver/downloads
-#/usr/local/bin
+# #https://sites.google.com/a/chromium.org/chromedriver/downloads
+# #/usr/local/bin
+# import selenium
+# # import os
+
+# from selenium import webdriver
+# # from selenium.webdriver.common.keys import Keys
+
+# PATH = '/usr/local/bin/chromedriver'
+# driver = webdriver.Chrome(PATH)
+# driver.get("https://www.melika.dev/")
+
+
 import selenium
 import os
 
@@ -7,4 +18,5 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 chromedriver = '/usr/local/bin/chromedriver'
-# browser = webdriver.Chrome(chromedriver)
+browser = webdriver.Chrome(chromedriver)
+browser.get('https://stackoverflow.com/users/login')
